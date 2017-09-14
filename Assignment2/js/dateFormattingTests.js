9558234f1a4df1c18e819a149fb446302d4711be
cafort <caleb.fort@gmail.com>
2017-09-14 14:43:56 -0500

    CSS and final javascript.

--- Assignment2/js/dateFormattingTests.js
+++ Assignment2/js/dateFormattingTests.js
@@ -1,61 +1,82 @@
 //Using an extra sample date to fail tests and to take advantage of the functions.
 var sampleDates = [
-    new Date('July 21, 2017, 22:31:31'),
-    new Date('September 05, 1992, 10:04:01'),
-    new Date('October 09, 1969, 18:15:01')
+    new Date("July 21, 2017, 22:31:31"),
+    new Date("September 05, 1992, 10:04:01"),
+    new Date("October 09, 1969, 18:15:01")
 ];
 
 //Third date for each method intentionally wrong
 var expectedResults = [
-    '10:31 PM', // Short Time 1 pass
-    '10:04 AM', //Short Time 2 pass
-    '6:15 pm', //Short Time 3 fail
-    '10:31:31 PM', //Long time 1 pass
-    '10:04:01 AM', // Long time 2 pass
-    '10:04 am', //Long time 3 fail
-    '7/21/2017', // Short date 1 pass
-    '9/5/1992', // Short date 2 pass
-    'October 9, 1969', // Short date 3 fail
-    'July 21, 2017', // Long date 1 pass
-    'September 5, 1992', // Long date 2 pas
-    '10/09/1969', //Long dae 3 fail
-    '7/21/2017 10:31 PM', //Short date time 1 pass
-    '9/5/1992 10:04 AM', // Short date time 2 pass
-    'october 9', //Long date 3 fail
-    'July 21, 2017 10:31 AM', // Long date time 1 pass
-    'September 5, 1992 10:04 AM', // Long date time 2 pass
-    'Octoer 9 10 a.m.', //Long date time 3 fail
-    'Friday, July 21, 2017, 6:15 PM', // Extended date time 1 pass
-    'Saturday, September 5, 1992, 10:04:01', // Extended date time 2 pass
-    'Thursday, Ocboter 9, 1996 10 am' // extended date time 3 fail
-
+    "10:31 PM", // Short Time 1 pass
+    "10:04 AM", //Short Time 2 pass
+    "6:15 pm", //Short Time 3 fail
+    "10:31:31 PM", //Long time 1 pass
+    "10:04:01 AM", // Long time 2 pass
+    "10:04 am", //Long time 3 fail
+    "7/21/2017", // Short date 1 pass
+    "9/5/1992", // Short date 2 pass
+    "October 9, 1969", // Short date 3 fail
+    "July 21, 2017", // Long date 1 pass
+    "September 5, 1992", // Long date 2 pas
+    "10/09/1969", //Long dae 3 fail
+    "7/21/2017 10:31 PM", //Short date time 1 pass
+    "9/5/1992 10:04 AM", // Short date time 2 pass
+    "october 9", //Long date 3 fail
+    "July 21, 2017 10:31 AM", // Long date time 1 pass
+    "September 5, 1992 10:04 AM", // Long date time 2 pass
+    "Octoer 9 10 a.m.", //Long date time 3 fail
+    "Friday, July 21, 2017, 6:15 PM", // Extended date time 1 pass
+    "Saturday, September 5, 1992, 10:04:01", // Extended date time 2 pass
+    "Thursday, Ocboter 9, 1996 10 am" // extended date time 3 fail
 ];
 
 var dateFormatterMethods = [
-    'getShortTime',
-    'getLongTime',
-    'getShortDate',
-    'getLongDate',
-    'getShortDateTime',
-    'getLongDateTime',
-    'getExtendedDateTime'
+    "getShortTime",
+    "getLongTime",
+    "getShortDate",
+    "getLongDate",
+    "getShortDateTime",
+    "getLongDateTime",
+    "getExtendedDateTime"
 ];
-
 
 //Create a function to compare the test date with predicted results using a method from dateFormatter. Return boolean and the inputs used.
 function testDate(date, predicted, method) {
     var result = dateFormatter[method](date);
     if (result === predicted) {
-        return 'Method: ' + method + ' | Expected result: ' + predicted + ' | Actual result: ' + result + ' | Matches: ' + true;
+        return (
+            "Method: " +
+            method +
+            " | Expected result: " +
+            predicted +
+            " | Actual result: " +
+            result +
+            " | Matches: " +
+            true
+        );
     } else {
-        return 'Method: ' + method + ' | Expected result: ' + predicted + ' | Actual result: ' + result + ' | Matches: ' + false;
-    };
-};
+        return (
+            "Method: " +
+            method +
+            " | Expected result: " +
+            predicted +
+            " | Actual result: " +
+            result +
+            " | Matches: " +
+            false
+        );
+    }
+}
 
-//Loop through each dateFormatter method with three test dates. 
+//Loop through each dateFormatter method with three test dates.
 for (var i = 0, i2 = 0; i < dateFormatterMethods.length; i++, i2 += 3) {
-    console.log(testDate(sampleDates[0], expectedResults[i2], dateFormatterMethods[i]));
-    console.log(testDate(sampleDates[1], expectedResults[i2 + 1], dateFormatterMethods[i]));
-    console.log(testDate(sampleDates[2], expectedResults[i2 + 2], dateFormatterMethods[i]));
-
-};
+    console.log(
+        testDate(sampleDates[0], expectedResults[i2], dateFormatterMethods[i])
+    );
+    console.log(
+        testDate(sampleDates[1], expectedResults[i2 + 1], dateFormatterMethods[i])
+    );
+    console.log(
+        testDate(sampleDates[2], expectedResults[i2 + 2], dateFormatterMethods[i])
+    );
+}
--- Assignment2/js/dateformatting.js
+++ Assignment2/js/dateformatting.js
@@ -27,26 +27,26 @@ var dateFormatter = {
     getShortTime: function (date) {
         var hour = date.getHours();
         var minutes = date.getMinutes();
-        minutes < 10 ? minutes = '0' + minutes : '' ;
+        minutes < 10 ? (minutes = "0" + minutes) : "";
         // If it's before noon, set to AM. If it's afternoon, subtract 12 from hours and add PM.
         if (hour < 12) {
             return hour + ":" + minutes + " AM";
         } else {
             return hour - 12 + ":" + minutes + " PM";
-        };
+        }
     },
     //Return date as 4:20:00 PM. Same as above plus seconds.
     getLongTime: function (date) {
         var hour = date.getHours();
         var minutes = date.getMinutes();
         var seconds = date.getSeconds();
-        minutes < 10 ? minutes = '0' + minutes : '' ;
-        seconds < 10 ? seconds = '0' + seconds : '' ;
+        minutes < 10 ? (minutes = "0" + minutes) : "";
+        seconds < 10 ? (seconds = "0" + seconds) : "";
         if (hour < 12) {
             return hour + ":" + minutes + ":" + seconds + " AM";
         } else {
             return hour - 12 + ":" + minutes + ":" + seconds + " PM";
-        };
+        }
     },
     //Return as 1/20/2017
     getShortDate: function (date) {
@@ -60,20 +60,22 @@ var dateFormatter = {
         var month = this.months[date.getMonth()];
         var day = date.getDate();
         var year = date.getFullYear();
-        return month + ' ' + day + ', ' + year;
+        return month + " " + day + ", " + year;
     },
     //Return as short date and time
     getShortDateTime: function (date) {
-        return this.getShortDate(date) + ' ' + this.getShortTime(date);
+        return this.getShortDate(date) + " " + this.getShortTime(date);
     },
 
     //Return long date and time
     getLongDateTime: function (date) {
-        return this.getLongDate(date) + ' ' + this.getLongTime(date);
+        return this.getLongDate(date) + " " + this.getLongTime(date);
     },
     //Return extended date and time, with day of the week
     getExtendedDateTime: function (date) {
         var weekDay = this.daysOfWeek[date.getDay()];
-        return weekDay + ', ' + this.getLongDate(date) + ', ' + this.getLongTime(date);
+        return (
+            weekDay + ", " + this.getLongDate(date) + ", " + this.getLongTime(date)
+        );
     }
 };
